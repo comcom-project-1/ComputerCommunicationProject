5a5f99574ae7c10d64403e7074fbaaf89deeadc9
Alperen Yucal <alperenyucal@gmail.com>
2020-12-22 13:27:51 +0300

    Check filename

--- server.py
+++ server.py
@@ -29,7 +29,7 @@ def unreliableSend(packet, sock, user, errRate):
 
 HOST = '127.0.0.1'  # The server's hostname or IP address
 PORT = 65432        # The port used by the server
-user = (HOST, PORT)
+server = (HOST, PORT)
 status = "Start"
 
 errRate = 10 # Average Error rate of the unreliable channel
@@ -37,15 +37,11 @@ TIMEOUT = 0.0001 # Timeout value
 N = 1 # Go-back-N N
 
 filename = b'crime-and-punishment.txt'
-
-"""
 passwd = Random.get_random_bytes(32)                        # AES256 must be 32 bytes
 secretWord = b"This word is secret"                         # The word that will be encrypted.  
 AEScipher = AES.new(passwd, AES.MODE_ECB)                   # Create AES cipher with given key. 
 phrase= AEScipher.encrypt(pad(secretWord))                  # The words that will be encrypted
                                                             # Must have length multiple of 16.
-print(phrase)
-print(AEScipher.decrypt(phrase).decode('utf-8'))
 
 rsaKey = RSA.generate(1024)                                 # Generate RSA public and Private keys
 private_key = rsaKey.export_key()                           # Export private key.
@@ -59,91 +55,37 @@ rsaDecryptor = PKCS1_OAEP.new(privateKey)                   # Which have differe
 
 enc = rsaEncryptor.encrypt(secretWord)#.encode('utf-8'))
 dec = rsaDecryptor.decrypt(enc)
-print(enc, dec)
-"""
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)     # Create UDP socket
-sock.settimeout(TIMEOUT)                                    # If no packets came after TIMEOUT
-                                                            # Then throw exception
-sock.bind(user)
+sock.bind(server)
 print("Server is running...")
 
 while True:
-    try:
-        data, addr = sock.recvfrom(1024)                 # print client ip
-        """if status == "Start":
-            # Create payload
-            length = toByte(len(filename) + len(public_key))
-            print(public_key)
-            print(len(filename) + len(public_key), length)  
-            pType = toByte(0)
-            packet = pType + length
-            packet += filename + public_key
-            # Send to the server
-            unreliableSend(packet, sock, user, errRate)
-            status = "Handshaking"
+        data, user = sock.recvfrom(1024)
+        print('Received:', data)
 
-        else:"""
-        if True:                                            # Bypass start state 
-            data, user = sock.recvfrom(1024)
-            print('Received:', data)
-            if status == "Handshaking":
-                enc = rsaDecryptor.decrypt(data)
-                if data[0] == 0:
-                    packetLength = data[1]
-                    sessionKey = rsaDecryptor.encrypt(data[2:2+packetLength])
-                    print("sessionKey =", sessionKey, packetLength)
+        if data[0] == 0:                                        # Handshake
+            packetLength = data[1]
+            recievedFileName = data[2:2+len(filename)]
+            if(recievedFileName != filename):
+                sock.settimeout()
+            recievedPublicKey = data[2+len(filename):2+packetLength]
+            recievedPublicKey = RSA.import_key(recievedPublicKey)
+            rsaEncryptor = PKCS1_OAEP.new(recievedPublicKey)
+            sessionKey = rsaEncryptor.encrypt(secretWord)
+            pType = toByte(0)                                   # Packet type
+            length = toByte(len(sessionKey))                    # Payload length
+            packet = pType + length + sessionKey                # Packet
+            unreliableSend(packet, sock, user, errRate)         # Send response to client
 
-                    # ACK segment 00
-                    packet = toByte(1) + toByte(0) 
-                    packet = pad(packet)
-                    print(packet)
-                    #print(bytes(packet))
-                    AEScipher = AES.new(sessionKey, AES.MODE_ECB)
-                    packet = AEScipher.encrypt(packet)
-                    unreliableSend(packet, sock, user, errRate)
-                    nextSeqNum = 0
+        elif data[0] == 1:                                      # ACK
+            pass
 
-                    status = "DataTransfer"
-                else:
-                    print("SERVER SENT WRONG PACKET")
-                    exit(1)
-            
-            elif status == "DataTransfer":
-                data = AEScipher.decrypt(data)
-                data = unpad(data)
-                packetType = data[0]
-                if packetType == 2:
-                    packetLenght = data[1]
-                    sequenceNumber = data[2]
+        elif data[0] == 2:                                      # DATA
+            pass
 
-                    packet = toByte(1) + toByte(sequenceNumber)
-                    packet = pad(packet)
-                    packet = AEScipher.encrypt(packet)
-                    unreliableSend(packet, sock, user, errRate)
+        elif data[0] == 3:                                      # FIN
+            pass
 
-                    if sequenceNumber == nextSeqNum: 
-                        payload = data[3:3+packetLenght].decode('utf-8')
-                        print(sequenceNumber, payload[:-1])
-                        nextSeqNum = (nextSeqNum + 1) % 256
-                        
-                    else:
-                        #print("Discarding packet", sequenceNumber, "expected", nextSeqNum)
-                        pass # if it is not expected discard the packet
-
-                elif packetType == 3:
-                    print("Transmission Complete")
-                    packet = toByte(1) + toByte(0)
-                    packet = pad(packet)
-                    packet = AEScipher.encrypt(packet)
-                    unreliableSend(packet, sock, user, errRate)
-                    status = "Ending"
-                    exit(0)
-                else:
-                    print("SERVER SENT WRONG PACKET")
-                    exit(1)
-    except Exception as ex:         
-        #print(ex)
-        if status == "Handshaking" or status == "Ending":
-            unreliableSend(packet, sock, user, errRate)
-
+        else:
+            print("CLIENT SENT WRONG PACKET")
